9873422180084afffe06c79ba9330f4f39067519
winson <929566854@qq.com>
2018-09-15 17:03:46 +0800

    add refresh listener;

--- app/src/main/java/com/winson/widget/pullrefreshlalayoutsample/LayoutDemo.java
+++ app/src/main/java/com/winson/widget/pullrefreshlalayoutsample/LayoutDemo.java
@@ -18,6 +18,14 @@ public class LayoutDemo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_demo);
 
+        final PullRefreshLayout pullRefreshLayout = findViewById(R.id.pull_refresh_layout);
+        pullRefreshLayout.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                pullRefreshLayout.refreshComplete(3000);
+            }
+        });
+
         ListView listView = findViewById(R.id.list_view);
         List<String> datas = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
@@ -32,6 +40,8 @@ public class LayoutDemo extends AppCompatActivity {
                 ((TextView) viewHolder.findViewById(R.id.title)).setText(obj);
             }
         });
+
+
 
     }
 }
